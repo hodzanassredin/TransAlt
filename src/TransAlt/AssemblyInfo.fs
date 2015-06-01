00b4fa4d856336db012bed1b673ad471b27bf566
Shelin Anton <shelinanton@gmail.com>
2015-06-01 16:41:42 +0300

    Bump version to 0.0.1-beta

--- src/TransAlt/AssemblyInfo.fs
+++ src/TransAlt/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("TransAlt")>]
 [<assembly: AssemblyProductAttribute("TransAlt")>]
 [<assembly: AssemblyDescriptionAttribute("This project is a proof of concept implementation of async computation workflows composition with non determenistic choice, merge and bind based on immutable state with lock detection. Uses ideas from Stm,Hopac,Joinads.")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "0.0.1"
